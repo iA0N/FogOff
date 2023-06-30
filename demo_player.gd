ea10582f8d7cc95d6d857d2089bc88081fa2c4cb
iA0N <ia0n@protonmail.com>
2023-06-30 18:07:25 +0200

    remake of the prototype map plus export of it

---
+++ demo_player.gd
@@ -0,0 +1,34 @@
+extends CharacterBody3D
+
+# How fast the player moves in meters per second.
+@export var speed = 14
+# The downward acceleration when in the air, in meters per second squared.
+@export var fall_acceleration = 75
+
+var target_velocity = Vector3.ZERO
+
+
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+
+	if Input.is_action_pressed("D"):
+		direction.x += 1
+	if Input.is_action_pressed("A"):
+		direction.x -= 1
+	if Input.is_action_pressed("S"):
+		direction.z += 1
+	if Input.is_action_pressed("W"):
+		direction.z -= 1
+
+	# Ground Velocity
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+
+	# Vertical Velocity
+	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+
+	# Moving the Character
+	velocity = target_velocity
+	move_and_slide()
+	self.position.y = 6
--- scenes/main.gd
+++ scenes/main.gd
@@ -1,10 +1,37 @@
 extends Node3D
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
+var better_lightning = true
+var iso = false
+@onready var env = $WorldEnvironment.environment
 
+func _ready():
+	print("test")
+	# ssil
+	# sdfgi
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	$Control/VBoxContainer/Label3.text = "FPS: " + str(Engine.get_frames_per_second())
+	
+	if Input.is_action_just_released("T"):
+		if better_lightning == true:
+			better_lightning = !better_lightning
+			$WorldEnvironment.environment.set_ssil_enabled(better_lightning)
+			$WorldEnvironment.environment.set_sdfgi_enabled(better_lightning)
+			$Control/VBoxContainer/Label4.text = "Toggle Lightning: T (OFF)"
+		else:
+			better_lightning = !better_lightning
+			$WorldEnvironment.environment.set_ssil_enabled(better_lightning)
+			$WorldEnvironment.environment.set_sdfgi_enabled(better_lightning)
+			$Control/VBoxContainer/Label4.text = "Toggle Lightning: T (ON)"
+
+	if Input.is_action_just_released("Q"):
+		if !iso:
+			$demo_player/Camera3D.current = false
+			$demo_player/Camera3D_ISO.current = true
+			iso = !iso
+		else:
+			$demo_player/Camera3D.current = true
+			$demo_player/Camera3D_ISO.current = false
+			iso = !iso
+			
